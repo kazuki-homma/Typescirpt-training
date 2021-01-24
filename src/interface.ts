a959415597964f06317a480bcde69740e2a6992d
kazuki-h <kazuki-h@weseek.co.jp>
2021-01-24 09:10:56 +0900

    readonly

--- src/interface.ts
+++ src/interface.ts
@@ -1,14 +1,28 @@
 interface Human {
-    name: string;
+    readonly name: string;
     age: number;
     greeting(message: string):void;
 }
+class Developer implements Human {
+    constructor(public name: string, public age: number, public experience: number) {
+    }
 
-const human = {
-    name: 'Quill',
-    age: 38,
     greeting(message: string) {
         console.log(message);
     }
 }
-let tmpFunc: (message: string) => void;
+const tmpDeveloper = {
+    name: 'Quill',
+    age: 38,
+    experience: 3,
+    greeting(message: string) {
+        console.log(message);
+    },
+}
+const user: Human = {
+    name: 'Quill',
+    age: 38,
+    greeting(message: string) {
+        console.log(message);
+    },
+}
