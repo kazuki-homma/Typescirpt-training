dd1514ebf89b711a1d49b907185cf070223496a4
kazuki-h <kazuki-h@weseek.co.jp>
2021-01-25 14:18:24 +0900

    typegurd in and instaceof

--- intersection.ts
+++ intersection.ts
@@ -29,3 +29,39 @@ function toUpperCase(x: string | number) {
     }
     return '';
 }
+
+type NomadWorker = Engineer | Blogger;
+function describeNomadWorkerProfile(nomadWorker: NomadWorker) {
+  console.log(nomadWorker.name);
+  if ('role' in nomadWorker) {
+      console.log(nomadWorker.role);
+  }
+  if ('follower' in nomadWorker) {
+      console.log(nomadWorker.follower);
+  }
+}
+
+class Dog {
+    speak() {
+        console.log('bow-wow');
+    }
+}
+
+class Bird {
+    speak() {
+        console.log('tweet-tweet');
+    }
+    fly() {
+        console.log('flutter');
+    }
+}
+
+type Pet = Dog | Bird;
+function havePet(pet: Pet) {
+    pet.speak();
+    if (pet instanceof Bird) {
+        pet.fly();
+    }
+}
+
+havePet(new Dog());
