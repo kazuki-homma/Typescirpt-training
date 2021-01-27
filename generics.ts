c17cd8f3d3740e104036af0f7fb8b75106222cca
kazuki-h <kazuki-h@weseek.co.jp>
2021-01-27 11:38:45 +0900

    keyof union

---
+++ generics.ts
@@ -0,0 +1,7 @@
+function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): T{
+    value[key];
+    return value;
+}
+
+console.log(copy({ name: 'Quill', age: 38}, 'age'));
+type K = keyof { name: string; age: number}
