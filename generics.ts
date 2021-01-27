442cccdf586cc5ca8079c7d4aa4749204bb952f6
kazuki-h <kazuki-h@weseek.co.jp>
2021-01-27 11:48:34 +0900

    class Generics

--- generics.ts
+++ generics.ts
@@ -5,3 +5,23 @@ function copy<T extends { name: string }, U extends keyof T>(value: T, key: U): 
 
 console.log(copy({ name: 'Quill', age: 38}, 'age'));
 type K = keyof { name: string; age: number}
+
+class LightDatabase<T extends string | number | boolean > {
+    private data: T[] = [];
+    add(item: T) {
+      this.data.push(item);
+    }
+    remove(item: T) {
+      this.data.splice(this.data.indexOf(item), 1)
+    }
+    get() {
+        return this.data;
+    }
+}
+const stringLightDatabase = new LightDatabase<string>();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Grape');
+stringLightDatabase.remove('Apple');
+console.log(stringLightDatabase.get());
+
