c91d7aef63f282d1cba28e6a352a17ee7bacff7c
kazuki-h <kazuki-h@weseek.co.jp>
2021-01-28 12:15:25 +0900

    HTMLCollection to array

--- dist/food-app.js
+++ dist/food-app.js
@@ -2,8 +2,22 @@
 class Score {
 }
 class Food {
+    constructor(element) {
+        this.element = element;
+        element.addEventListener('click', this.clickEventHandler.bind(this));
+    }
+    clickEventHandler() {
+        this.element.classList.toggle('food--active');
+    }
 }
 class Foods {
+    constructor() {
+        this.elements = document.getElementsByClassName('food');
+        const foods = Array.from(this.elements);
+        foods.forEach(element => {
+            new Food(element);
+        });
+    }
 }
-Foods.elements = document.querySelector('.food');
-console.log(Foods.elements);
+const foods = new Foods();
+console.log(foods.elements);
--- src/food-app.ts
+++ src/food-app.ts
@@ -1,19 +1,21 @@
 class Score { }
 class Food {
-    constructor(public element: HTMLDivElement) {
-        element.addEventListener('click', this.clickEventHandler);
+    constructor(public element: Element) {
+        element.addEventListener('click', this.clickEventHandler.bind(this));
     }
     clickEventHandler() {
         this.element.classList.toggle('food--active');
     }
  }
 class Foods {
-    elements = document.querySelector<HTMLDivElement>('.food');
+    elements = document.getElementsByClassName('food');
     constructor() {
-        this.elements.forEach(element => {
+        const foods = Array.from(this.elements);
+        foods.forEach(element => {
             new Food(element);
         });
     }
 }
 
 const foods = new Foods();
+console.log(foods.elements);
