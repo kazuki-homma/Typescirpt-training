c5910426a752b8cd287aec089a1f5fb2cdb5cf64
kazuki-h <kazuki-h@weseek.co.jp>
2021-01-28 11:35:02 +0900

    class separate

--- src/food-app.ts
+++ src/food-app.ts
@@ -1,9 +1,14 @@
 class Score { }
-class Food { }
+class Food {
+    constructor(public element: HTMLDivElement) {
+        element.addEventListener('click', );
+    }
+ }
 class Foods {
     elements = document.querySelector<HTMLDivElement>('.food');
     constructor() {
-        this.elements.forEach(element => {
+        this.elements!.forEach(element => {
+            new Food(element);
         });
     }
 }
