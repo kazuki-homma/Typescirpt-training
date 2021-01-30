d959402291c7ca73fec6cff261f1ebf5ed215beb
kazuki-h <kazuki-h@weseek.co.jp>
2021-01-30 14:54:13 +0900

    train

---
+++ train/src/components/hello.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type HelloProps = {
+    message: string,
+}
+
+const Hello = (props: HelloProps) => {
+    return (
+        <h1>こんにちは{props.message}</h1>
+    )
+}
+
+export default Hello;
