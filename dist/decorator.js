17ef2ae9a7f31b65ecdeaad59960dd3a35e2906f
kazuki-h <kazuki-h@weseek.co.jp>
2021-02-01 15:06:17 +0900

    new class decolator

---
+++ dist/decorator.js
@@ -0,0 +1,32 @@
+"use strict";
+// udemy
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// デコレーターとはClassにつけられる関数のこと
+// tsconfig.jsonのexperimental decolatorをtrueにする必要がある。(将来的にjsに実装されるため)
+// クラスにデコレーションするかメソッドにデコレーションするかによって書く場所が変わる
+// クラスデコレーターはインスタンスではなく、クラス定義時に実行される。
+function Logging(constructor) {
+    console.log('logging');
+}
+var User = /** @class */ (function () {
+    function User() {
+        this.name = 'Quill';
+        console.log('User was created!');
+    }
+    User = __decorate([
+        Logging
+    ], User);
+    return User;
+}());
+var user = new User();
+// 複数のデコレーターを同時に使う
+// 戻り値にクラスを指定して新しいClassを作る
+// プロパティデコレーターを使う方法
+// メソッドデコレータ & PropertyDescripter
+// アクセサーデコレーター
+// パラメータデコレーター
---
+++ dist/framework.js
@@ -0,0 +1,34 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// デコレーターを使ってフレームワークを作成する
+// htmlのidに特定の文字を表示する
+// classのnameを引き出すにはconstructorが必要だがそれをするにはinstanceの生成が必要 { new(): object }と言う型で
+// さらにそのobjectの中のnameなどを使う場合、objectを{ name: string } などにしなくてはいけない
+// constructorに引数があるときはデコレーターの型のnew()にも同じものを入れる。
+// いろいろなクラスに適応する場合は...args: any[]のようにすることでどのconstructorにも対応
+function Component(template, selector) {
+    return function (constructor) {
+        var mountedElement = document.querySelector(selector);
+        var instance = new constructor();
+        if (mountedElement) {
+            mountedElement.innerHTML = template;
+            mountedElement.querySelector('h1').textContent = instance.name;
+        }
+    };
+}
+var User2 = /** @class */ (function () {
+    function User2() {
+        this.name = 'QQQuill';
+        console.log('User was created!');
+    }
+    User2 = __decorate([
+        Component('<h1>{{ name }}</h1>', '#app')
+    ], User2);
+    return User2;
+}());
+var user2 = new User2();
